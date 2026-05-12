342afd59d5ef1da4ab41b41326654726e98ea45f
GeniusMachado <geniusmachado@gmail.com>
2026-05-12 18:30:40 -0400

    first commit from Kali Linux on iBall Compbook M500

---
+++ kalitoday.py
@@ -0,0 +1,13 @@
+import kalilinux
+def flashed_iBall_compbook_m500:
+	print("better UI from antix linux")
+	break
+
+	for i in range(6:00pm):
+		while(working):
+			print("clocked Out")
+	
+if __name__ = "__main__":
+	print("spoke with Karan M about BGV & Hartford")
+
+
