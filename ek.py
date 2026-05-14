263f70fa5a71a45aab3add55c7a4c629cc8e9576
GeniusMachado <geniusmachado@gmail.com>
2026-05-14 11:26:44 -0400

    second commit from Kali Linux on iBall Compbook M500

---
+++ ek.py
@@ -0,0 +1,49 @@
+jfsoig
+jkdgijd
+fdjg
+
+aja fgfgf
+
+
+
+j
+j
+ s
+
+
+ i
+  sg uu gdh sdgu df dd ja
+  aja
+  jfd
+  jagg d
+   g
+   j a 
+   g
+    a g jad  a
+     df
+     ad 
+     h
+     k
+     ddaf
+     a d
+     ja
+     jkj
+     kadf
+     'f
+     s d'hd s'osdj adfg
+     'z 
+     ja g ' f 'jd 
+     j'
+
+
+     s sf
+
+     erggg 
+gffsd g
+
+
+
+
+
+s
+fgb
