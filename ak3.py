771690ad4d89c71cb62ff8a6cb9edf410df0bf24
GeniusMachado <geniusmachado@gmail.com>
2026-05-20 16:38:20 -0400

    Azi's graduation comencement 2026

---
+++ ak3.py
@@ -0,0 +1,16 @@
+print("we out here in Hiilton")
+memories = [2022, 2023, 2024, 2025, 2026]
+def Laguardia():
+    graduation = "Azi Khawaja"
+    year = 2026
+    undergrads = True
+    for year in memories : 
+        print("time spent together with gym workouts and brotherhood")
+        break
+
+Laguardia()
+
+if __name__ = "__main__"
+    print(f"happy and proud moment in {year} to meet his family at graduation")
+
+
