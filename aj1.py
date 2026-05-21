3796c9ab10f9666cb9135f8420ded40be3edf952
GeniusMachado <geniusmachado@gmail.com>
2026-05-21 15:46:53 -0400

    Nervous news about BGV

---
+++ aj1.py
@@ -0,0 +1,10 @@
+print("nervous response from EXL")
+
+def Surabhi(num): 
+    i = self.num
+    for i in range(20):
+        print("Surabhi gave bad news and no hopes from the leads about bgv")
+
+Surabhi(11)
+
+
